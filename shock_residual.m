6155ec723b17868d4b156d40759c5c3e9cbdbcf7
Joseph Le <josephle328@gmail.com>
2026-09-18 11:58:18 -0600

    Update MAATLAB files'

--- shock_residual.m
+++ shock_residual.m
@@ -1,15 +1,10 @@
-function shock_residual(delta, theta, M)
+function residual = shock_residual(delta, theta, M)
 
 gamma = 1.4;
 
-mu = asind(1/M);
-
-   if mu <= theta && theta <= 90
-        shock_residual = NaN; 
-        return;
-    end
-
-shock_residual = tand(delta) - (M.^2 * sind(2*theta) - 2*cotd(theta)) / (2+(M.^2 * (gamma + cosd(2*theta))));
-
+numerator = M^2 * sind(2*theta) - 2*cotd(theta);
+denominator = 2+(M^2 * (gamma + cosd(2*theta)));
+    
+    residual = tand(delta) - (numerator ./ denominator);
 
 end
