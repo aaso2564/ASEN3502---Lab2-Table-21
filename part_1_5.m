90315beb4a2e42a4f64fd5c815988c1886589c63
Joseph Le <josephle328@gmail.com>
2026-09-25 12:04:30 -0600

    Update MAATLAB files

--- part_1_5.m
+++ part_1_5.m
@@ -32,11 +32,20 @@ f_func = @(theta) shock_residual(delta, theta, M1);
 
 x0 = incremental_search(f_func, @x , [0 100], .1, 100);
 x1 = Root_Finding_Secant_(f_func, @x , [30 50], .1, 100);
+x2 = bisection(f_func, @x , [30 50], .1, 100);
+x3 = newton_raphson(f_func, @x , [30 50], .1, 100);
 
 plot(x0, 0, 'ro', 'MarkerFaceColor','auto')
 text(x0, 0, '  Root1', 'VerticalAlignment', 'top')
+
 plot(x1, 0, 'ro', 'MarkerFaceColor','auto')
 text(x1, 0, '  Root2', 'VerticalAlignment', 'top')
+
+plot(x2, 0, 'ro', 'MarkerFaceColor','auto')
+text(x2, 0, '  Root3', 'VerticalAlignment', 'top')
+
+plot(x3, 0, 'ro', 'MarkerFaceColor','auto')
+text(x3, 0, '  Root4', 'VerticalAlignment', 'top')
 
 plot(theta, f_theta, 'r', 'LineWidth', 2)
 
